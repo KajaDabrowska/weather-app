1c09332b00ff08e8e3068e178f986506505d4876
KajaDabrowska <dabrowska.kaja.z@gmail.com>
2022-05-06 14:57:52 +0200

    added all info for weather now, city name and date, some styling

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,208 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Fragment } from "react";
+import { useState, useEffect } from "react";
 
-function App() {
+import Image from "./components/image/image.component";
+
+import dropImage from "./images/image-drop.svg";
+import windImage from "./images/image-wind.svg";
+import cloudImage from "./images/image-cloud.svg";
+
+import "./App.scss";
+
+//{ latitude: lat, longitude: lng }
+export type Coords = {
+  lat: number | null;
+  lng: number | null;
+};
+
+// const API_KEY = "47ecb06584efcd3a09d06d7e70fd2cb8";
+const API_KEY = "3585e187fe2dcd51bd3ecd35186e4637";
+
+const App = () => {
+  const [coords, setCoords] = useState<Coords>({ lat: null, lng: null });
+  const [cityName, setCityName] = useState<string | null>(null);
+  const [date, setDate] = useState("null");
+  const [nowTemp, setNowTemp] = useState("null");
+  const [nowDesc, setNowDesc] = useState("null");
+  const [nowIcon, setNowIcon] = useState("null");
+
+  const [nowHumid, setNowHumid] = useState("null");
+  const [nowWind, setNowWind] = useState("null");
+  const [nowClouds, setNowClouds] = useState("null");
+
+  //TODO error msg if geolocation not available
+  useEffect(() => {
+    if ("geolocation" in navigator) {
+      // console.log("Geolocation available");
+
+      getPosition();
+    } else {
+      console.log("Not Available");
+    }
+  }, []);
+
+  useEffect(() => {
+    getDate();
+  }, []);
+
+  useEffect(() => {
+    getCity();
+  }, [coords]);
+
+  useEffect(() => {
+    // console.log("coords changed so i can fetch weather data");
+
+    getWeatherNow();
+  }, [coords]);
+
+  const getPosition = async () => {
+    try {
+      await navigator.geolocation.getCurrentPosition((position) =>
+        setCoords({
+          lat: position.coords.latitude,
+          lng: position.coords.longitude,
+        })
+      );
+    } catch (err) {
+      console.log(err, "could not get location");
+    }
+  };
+
+  const getDate = () => {
+    const now = new Date();
+
+    // const day = now.toLocaleString("default", { weekday: "short" });
+    // const dayNum = now.toLocaleString("default", { day: "numeric" });
+    // const month = now.toLocaleString("default", { month: "short" });
+    const all = now.toLocaleString("default", {
+      weekday: "short",
+      day: "numeric",
+      month: "short",
+    });
+
+    // console.log("now:", now);
+    // console.log("day:", day);
+    // console.log("day numb:", dayNum);
+    // console.log("month:", month);
+    // console.log("all: ", all);
+    // console.log(all);
+    setDate(all);
+  };
+
+  const capitalizeFirstLetter = (string: string) => {
+    return string.charAt(0).toUpperCase() + string.slice(1);
+  };
+
+  const getWeatherNow = async () => {
+    try {
+      const { lat, lng } = coords;
+
+      if (lat === null) return;
+
+      const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lng}&units=metric&appid=${API_KEY}`;
+      // console.log(url);
+
+      const res = await fetch(url);
+      const data = await res.json();
+
+      console.log("Weather data: ", data);
+      setNowTemp(data.main.temp);
+      setNowDesc(data.weather[0].description);
+      setNowIcon(data.weather[0].icon);
+
+      setNowWind(data.wind.speed);
+      setNowHumid(data.main.humidity);
+      setNowClouds(data.clouds.all);
+    } catch (err) {
+      //TODO error msg
+      console.log(err, "error fetching WEATHER data");
+    }
+  };
+
+  const getCity = async () => {
+    try {
+      const { lat, lng } = coords;
+      const url = `https://api.bigdatacloud.net/data/reverse-geocode-client?latitude=${lat}&longitude=${lng}&localityLanguage=en`;
+
+      const res = await fetch(url);
+      const data = await res.json();
+
+      // console.log("City data: ", data);
+
+      setCityName(data.city);
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <Fragment>
+      <header className="header--container container">
+        <h1 className="city">{cityName}</h1>
+        <p className="date">{date}</p>
       </header>
-    </div>
+
+      <main className="container">
+        <div className="main--container glass">
+          <div className="big--container ">
+            <div className="tempDesc">
+              <div className="temp">
+                {nowTemp}° <sup>C</sup>
+              </div>
+              <div className="desc">{capitalizeFirstLetter(nowDesc)}</div>
+            </div>
+
+            {/*@ts-ignore  */}
+            <Image imageCode={nowIcon ? nowIcon : null} />
+          </div>
+          <div className="small--container">
+            <div className="details">
+              <img src={windImage} alt="" className="image" />
+              {/*@ts-ignore  */}
+              <p>{Math.trunc(nowWind)} km/h</p>
+            </div>
+
+            <div className="details">
+              <img src={dropImage} alt="" className="image" />
+              <p>{nowHumid}%</p>
+            </div>
+
+            <div className="details">
+              <img src={cloudImage} alt="" className="image" />
+              <p>{nowClouds}%</p>
+            </div>
+          </div>
+        </div>
+      </main>
+
+      {/* <main>
+      <h1>Hi</h1>
+
+      <p>
+        coords: {coords.lat}, {coords.lng}
+      </p>
+
+      <p>City name: {cityName}</p>
+
+      <p>data: {date}</p>
+
+      <p>
+        now weather: {nowTemp}, {nowDesc}, {nowIcon}
+      </p>
+
+      <Image imageCode={nowIcon} />
+    </main> */}
+    </Fragment>
   );
-}
+};
 
 export default App;
+
+// get city
+//https://api.bigdatacloud.net/data/reverse-geocode-client?latitude=52.1391643&longitude=21.0426115&localityLanguage=en
+// "city": "Warsaw",
+//  "countryName": "Poland",
+
+// get day
+
+// get 7 days
---
+++ src/components/image/image.component.jsx
@@ -0,0 +1,43 @@
+import sunImage from "../../images/image-sun.svg";
+import moonImage from "../../images/image-moon.svg";
+import cloudImage from "../../images/image-cloud.svg";
+import cloudsImage from "../../images/image-clouds.svg";
+import partCloudDayImage from "../../images/image-part-cloud-day.svg";
+import partCloudNightImage from "../../images/image-part-cloud-night.svg";
+import rainImage from "../../images/image-rain.svg";
+import lightningImage from "../../images/image-lightning.svg";
+import snowImage from "../../images/image-snow.svg";
+import mistyImage from "../../images/image-misty.svg";
+
+import "./image.styles.scss";
+
+const Image = (imageCode) => {
+  if (!imageCode) return;
+
+  const images = {
+    "01d": sunImage,
+    "01n": moonImage,
+    "02d": partCloudDayImage,
+    "02n": partCloudNightImage,
+    "03d": cloudImage,
+    "03n": cloudImage,
+    "04d": cloudsImage,
+    "04n": cloudsImage,
+    "09d": rainImage,
+    "09n": rainImage,
+    "10d": rainImage,
+    "10n": rainImage,
+    "11d": lightningImage,
+    "11n": lightningImage,
+    "13d": snowImage,
+    "13n": snowImage,
+    "50d": mistyImage,
+    "50n": mistyImage,
+  };
+
+  const image = images[Object.values(imageCode)];
+
+  return <img src={image} alt="" className="image--main" />;
+};
+
+export default Image;
